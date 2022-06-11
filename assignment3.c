d1f0b52996e257450289853ce7bfe7073d2677db
haziq7878 <63796888+haziq7878@users.noreply.github.com>
2022-06-11 10:15:18 +0500

    File handling of grammer
    3d array initialized

--- assignment3.c
+++ assignment3.c
@@ -27,8 +27,45 @@ char break_prod(char *prod)
     char ans[count][max];
 }
 
+// state counter
+int state_counter = 0;;
+char state_list[100][100][100] = {{'\0'}}; // state_list
+
+
+
 int main()
 {
+    FILE *ptr;
+    ptr = fopen("grammer.txt", "r");
+    char temp[80]; // temp array used for copying a single line from a file
+    if (ptr == NULL)
+    {
+        printf("There was an error in opening the file");
+        return -1;
+    }
+    int count = 0;
+    int production_index = 0;
+    while (fgets(temp, sizeof(temp), ptr) != NULL)
+    {
+        for (int i = 0; i < strlen(temp); i++)
+        {
+            if (temp[i] != '\n')
+            {
+                state_list[0][production_index][count] = temp[i];
+                count++;
+            }
+            else
+            {
+                production_index++;
+            }
+        }
+        count = 0;
+        memset(temp, 0, 80);
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%s\n", state_list[0][i]);
+    }
     char grammer[3][9] =
         {
             "S->L=R|R\0",
